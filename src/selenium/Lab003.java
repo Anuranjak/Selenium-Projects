1f5ec469ee2f2cccb75cfcaea87fe12ac634f780
Anuranjak Bhatt <bhattanuranjak@gmail.com>
2025-02-08 18:24:58 +0530

    Push success

--- src/selenium/Lab003.java
+++ src/selenium/Lab003.java
@@ -1,43 +1,44 @@
+//Open Flipkart --> Electronics --> Search Powerbank --> high to low --> print title of first highest power bank
 package selenium;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.interactions.Actions;
 import java.time.Duration;
+import java.util.List;
 
 public class Lab003 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://amityonline.com/amity/home/eligibility?f=5245045");
+        driver.get("https://www.flipkart.com/");
         driver.manage().window().maximize();
-        WebElement searchButton = driver.findElement(By.className("header_login__wzVUA"));
-        searchButton.click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4)); // Adjust timeout as needed
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3)); // Adjust timeout as needed
         try {
-            WebElement formElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("Form_control__eYGP8"))); // Replace with the actual locator
-            System.out.println("Form page loaded!");
-            // Now you can interact with the form
+            WebElement loginPage = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("_30XB9F"))); // Replace with the actual locator
+            System.out.println("Login cancelled");
         } catch (TimeoutException e) {
-            System.out.println("Form page did not load within the timeout: " + e.getMessage());
-            // Handle the timeout (take screenshot, log error, fail the test)
+            System.out.println("No alert recorded: " + e.getMessage());
         }
-        WebElement email = driver.findElement(By.cssSelector("[placeholder='Enter Email Id']"));
-        email.sendKeys("anuranjak@amityonline.com");
-        WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("AU12201996");
-        WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"_modal\"]/div/div/div/form/button"));
-        submitButton.click();
-        System.out.println("Login Successful");
+        WebElement elementToHoverOver = driver.findElement(By.xpath("//*[text()='Electronics']")); // Replace with your element locator
+        Actions actions = new Actions(driver);
+        actions.moveToElement(elementToHoverOver).perform();
+        WebElement powerBankLink = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Powerbank')]")));
+        System.out.println("element found");
+        WebElement elementPowerBank = driver.findElement(By.xpath("//*[text()='Powerbank']")); // Replace with your element locator
+        elementPowerBank.click();
+        WebElement sorting = driver.findElement(By.xpath("//div[@class='zg-M3Z' and text()='Price -- High to Low']"));
+        sorting.click();
+        List<WebElement> listItems =  driver.findElements(By.cssSelector("a.wjcEIp"));
+        System.out.println(listItems.get(0).getText());
         try {
-            Thread.sleep(Long.parseLong("5000"));
+            Thread.sleep(3000); // Sleep for 3 seconds
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            Thread.sleep(Long.parseLong("5000"));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            // Handle the interruption (e.g., log it, exit the loop, etc.)
+            System.err.println("Thread interrupted: " + e.getMessage());
         }
         driver.quit();
     }
